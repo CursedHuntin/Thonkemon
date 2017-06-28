8482e94350cab178439eea23680bd61dd6a335fe
IWummi <sebis100@uni-duesseldorf.de>
2017-06-28 21:42:57 +0200

    GET M0r€ Canc€r C0De GU1 1ust start€d

--- Thonkemon/src/game/Selection.java
+++ Thonkemon/src/game/Selection.java
@@ -1,10 +1,80 @@
 package game;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class Selection {
-	public Selection(Player p1, Player p2) {
-		while (true) {
-			actionSelection(p1, p2);
-		}
+
+	private Stage stage = Startup.primaryStage;
+	public static boolean sucess = true;
+	public boolean play = false;
+	private Stage primaryStage;
+	private Scene scene;
+
+	@FXML
+	private Button Load;
+	@FXML
+	private Button NoLoad;
+	@FXML
+	private Button Fight;
+	@FXML
+	private Button Items;
+	@FXML
+	private Button Thonkemon;
+	@FXML
+	private Button Escape;
+
+	// public Selection(Player p1, Player p2) {
+	// while (true) {
+	// actionSelection(p1, p2);
+	// }
+	// }
+
+	@FXML
+	protected void handleLoadButton(ActionEvent event) {
+		// hier soll das Laden einer bereits existierenden Datei implementiert
+		// werden.
+		Items.setVisible(true);
+		Escape.setVisible(true);
+		Thonkemon.setVisible(true);
+		Fight.setVisible(true);
+		Load.setVisible(false);
+		NoLoad.setVisible(false);
+
+	}
+
+	@FXML
+	protected void handleNoLoadButton(ActionEvent event) {
+		// Neuer Spielstand
+		Items.setVisible(true);
+		Escape.setVisible(true);
+		Thonkemon.setVisible(true);
+		Fight.setVisible(true);
+		Load.setVisible(false);
+		NoLoad.setVisible(false);
+	}
+
+	@FXML
+	protected void handleFightButton(ActionEvent event) {
+		// Kampf ausgewählt
+	}
+
+	@FXML
+	protected void handleEscapeButton(ActionEvent event) {
+		// Flucht
+	}
+
+	@FXML
+	protected void handleItemsButton(ActionEvent event) {
+
+	}
+
+	@FXML
+	protected void handleThonkemonButton(ActionEvent event) {
+
 	}
 
 	private void actionSelection(Player p1, Player p2) {
@@ -34,5 +104,13 @@ public class Selection {
 			} else
 				System.out.println("Wrong Input!");
 		}
+	}
+
+	public void setStage(Stage stage) {
+		primaryStage = stage;
+	}
+
+	public void setScene(Scene scene) {
+		this.scene = scene;
 	}
 }
--- Thonkemon/src/game/Startup.java
+++ Thonkemon/src/game/Startup.java
@@ -1,11 +1,49 @@
 package game;
 
-public class Startup {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Startup extends Application {
+	static Stage primaryStage;
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Startup.primaryStage = primaryStage;
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/Anfang.fxml"));
+			Parent root = (Parent) loader.load();
+			Selection con = (Selection) loader.getController();
+			con.setStage(primaryStage);
+			Scene scene = new Scene(root, 1000, 600);
+			con.setScene(scene);
+			String sheet = getClass().getResource("/application.css").toExternalForm();
+			scene.getStylesheets().add(sheet);
+			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
+			primaryStage.setTitle("Thonkemon");
+			primaryStage.centerOnScreen();
+			primaryStage.show();
+			Player p1 = new Player();
+			Player p2 = new Player();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) {
-		// new Fight(new Player());
-		Player p1 = new Player();
-		Player p2 = new Player();
-		new Selection(p1, p2);
-		// Kappas
+		launch(args);
 	}
 }
+
+// public class Startup {
+// public static void main(String[] args) {
+// new Fight(new Player());
+// Player p1 = new Player();
+// Player p2 = new Player();
+// new Selection(p1, p2);
+// Kappas
+// }
+// }//
