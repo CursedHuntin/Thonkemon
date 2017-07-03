8b3fde0b59b7eba1c6de042577cb03038428d7d3
sebis100 <sebis100@uni-duesseldorf.de>
2017-07-03 06:58:04 +0200

    Clog
    "noch pünktlich"

---
+++ projekt5/clog/Hauptmenue.java
@@ -0,0 +1,19 @@
+
+
+public class Hauptmenue {
+
+	public static void haupt() {
+
+		Konsole.println("Treffe eine Wahl!");
+		Konsole.println("");
+		Konsole.println("1) Clog Eintrag erzeugen");
+		Konsole.println("2) Clog ausgeben");
+		Konsole.println("3) Clog laden");
+		Konsole.println("4) Clog speichern");
+		Konsole.println("5) Programm beenden");
+		Konsole.println("");
+		Konsole.println("Bitte Entertaste bedienen");
+
+	}
+
+}
